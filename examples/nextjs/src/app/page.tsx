50563d6639fc5088949cb1898c2895eeee0dae78
henry-pulver <henrypulver@hotmail.co.uk>
2023-08-07 10:39:58 +0100

    Make example a mocked-up CRM

--- examples/nextjs/src/app/page.tsx
+++ examples/nextjs/src/app/page.tsx
@@ -1,122 +1,90 @@
 "use client";
-import Image from "next/image";
 import { SuperflowsButton } from "@superflows/chat-ui-react";
+import { Cog6ToothIcon } from "@heroicons/react/24/outline";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-12 pt-16">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <SuperflowsButton
-          superflowsApiKey={process.env.NEXT_PUBLIC_SUPERFLOWS_API_KEY}
-          styling={{
-            slideoverSide: "right",
-            headerTextColor: "#146ef5",
-            buttonColor: "#146ef5",
-          }}
-        />
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className="z-10 fixed left-0 inset-y-0 w-60 px-6 py-10 bg-gray-600 flex flex-col ">
+          <div className="flex flex-row gap-x-4">
+            <div className="rounded-full bg-gray-500 h-8 w-8 text-gray-800 flex place-items-center justify-center text-lg font-sans">
+              A
+            </div>
+            <div className="text-gray-200 text-2xl font-sans">Acme CRM</div>
+          </div>
+          <div className="mt-16 flex flex-col animate-pulse">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((n) => (
+              <>
+                {n !== 1 && <div className="w-full h-px bg-gray-500" />}
+                <div className="flex flex-row my-4">
+                  <div className="h-6 bg-gray-400 rounded-full w-full" />
+                </div>
+              </>
+            ))}
+          </div>
+          <div className="pt-24 animate-pulse flex space-x-4">
+            <div className="rounded-full bg-gray-200 h-10 w-10" />
+            <div className="flex-1 space-y-6 py-1">
+              <div className="h-2 bg-gray-200 rounded" />
+              <div className="space-y-3">
+                <div className="grid grid-cols-3 gap-4">
+                  <div className="h-2 bg-gray-200 rounded col-span-2" />
+                  <div className="h-2 bg-gray-200 rounded col-span-1" />
+                </div>
+                <div className="h-2 bg-gray-200 rounded" />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          className={
+            "z-0 pl-60 flex flex-row justify-end fixed top-0 inset-x-0 h-8 bg-gray-400 pr-20 gap-x-3"
+          }
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          {/*          Henry is a data scientist at Chatterbank.*/}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          {/*Project id is 11090 with key demo_superflows_teams.*/}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          {/*The most recent Chatterbank version is 7.27*/}
+          <SuperflowsButton
+            superflowsApiKey={process.env.NEXT_PUBLIC_SUPERFLOWS_API_KEY}
+            // superflowsUrl={process.env.NEXT_PUBLIC_SUPERFLOWS_API_URL}
+            AIname={"Superflows Assistant"}
+            styling={{
+              slideoverSide: "right",
+              // headerTextColor: "#146ef5",
+              // buttonColor: "#146ef5",
+            }}
+            mockApiResponses={true}
+            suggestions={[
+              "When am I meeting Alex from Alphabet Inc?",
+              "Who was the top-performing sales rep last quarter?",
+              "What's the most valuable open opportunity?",
+            ]}
+          />
+          {[1, 2, 3].map((n) => (
+            <div key={n} className="m-1 h-5 w-5 bg-gray-500 rounded-full" />
+          ))}
+          <Cog6ToothIcon className="z-0 w-7 h-7 p-1 text-gray-500 cursor-pointer hover:text-blue-600" />
+        </div>
+        <div className="ml-24 w-full h-full flex flex-col rounded-md">
+          <div className="text-2xl text-gray-600 mb-6 w-44 rounded-full h-7 animate-pulse bg-gray-400" />
+          {[
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+          ].map((n) => (
+            <div
+              key={n}
+              className="w-full bg-gray-100 border-b border-gray-400 animate-pulse flex space-x-4 px-6"
+            >
+              <div className="rounded-full bg-gray-500 h-8 my-2 w-8" />
+              <div className="rounded-full bg-gray-500 h-4 my-4 w-64" />
+              <div className="rounded-full bg-gray-500 h-4 my-4 w-28" />
+              <div className="rounded-full bg-gray-500 h-4 my-4 w-96" />
+              <div className="rounded-full bg-gray-500 h-4 my-4 w-40" />
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
