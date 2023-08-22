b4356a741f94d233eee4d14c32e1292959d06da3
neuromantic99 <jrowlandm@gmail.com>
2023-08-22 16:09:40 +0400

    adding processing for datetime on graphs

--- src/components/graph.tsx
+++ src/components/graph.tsx
@@ -8,20 +8,111 @@ import {
   YAxis,
 } from "recharts";
 
+import { DateTime } from "luxon";
+
+let formats = [
+  "yyyy-MM-dd'T'HH:mm:ss.SSSZ",
+  "yyyy-MM-dd'T'HH:mm:ss.SSS",
+  "yyyy-MM-dd'T'HH:mm:ss",
+  "yyyy-MM-dd'T'HH:mm",
+  "yyyy-MM-dd'T'HH",
+  "yyyy-MM-dd",
+  "yyyy/MM/dd'T'HH:mm:ss.SSSZ",
+  "yyyy/MM/dd'T'HH:mm:ss.SSS",
+  "yyyy/MM/dd'T'HH:mm:ss",
+  "yyyy/MM/dd'T'HH:mm",
+  "yyyy/MM/dd'T'HH",
+  "yyyy/MM/dd",
+  "yyyy.MM.dd'T'HH:mm:ss.SSSZ",
+  "yyyy.MM.dd'T'HH:mm:ss.SSS",
+  "yyyy.MM.dd'T'HH:mm:ss",
+  "yyyy.MM.dd'T'HH:mm",
+  "yyyy.MM.dd'T'HH",
+  "yyyy.MM.dd",
+  "yyyyMMdd'T'HHmmss.SSSZ",
+  "yyyyMMdd'T'HHmmss.SSS",
+  "yyyyMMdd'T'HHmmss",
+  "yyyyMMdd'T'HHmm",
+  "yyyyMMdd'T'HH",
+  "yyyyMMdd",
+  "HH:mm:ss.SSS",
+  "HH:mm:ss",
+  "HH:mm",
+  "MM-dd-yyyy",
+  "dd.MM.yyyy",
+  "MM/dd/yyyy",
+  "dd/MM/yyyy",
+  "h:mm a", // 1:30 PM
+];
+
+function attemptDatetimeConversion(array: any[]): number[] | null {
+  if (typeof array[0] !== "string") return null;
+
+  let matchingFormat = null;
+
+  for (const format of formats) {
+    let dt = DateTime.fromFormat(array[0], format);
+    if (dt.isValid) {
+      matchingFormat = format;
+      break;
+    }
+  }
+
+  if (!matchingFormat) return null;
+
+  const convertedArray = [];
+
+  for (const str of array) {
+    let dt = DateTime.fromFormat(str, matchingFormat);
+    if (!dt.isValid) {
+      return null;
+    }
+    convertedArray.push(dt.toSeconds());
+  }
+
+  return convertedArray;
+}
+
 export interface GraphData {
   data: { x: number | string; y: number }[];
   xLabel?: string;
   yLabel?: string;
   graphTitle?: string | number;
+  xIsdate?: boolean;
 }
 
 export function Graph(props: GraphData) {
   // Title currently does nothing in recharts, though there's an open
   // ticket for it
+
+  const xRange =
+    Math.max(...props.data.map((obj) => obj.x as number)) -
+    Math.min(...props.data.map((obj) => obj.x as number));
+
+  // This doesn't seem to be working for some reason
+  const offset = Math.ceil(xRange * 0.1);
+
   return (
     <ResponsiveContainer width="80%" aspect={2} className="sf-mx-auto sf-mt-2">
       <LineChart data={props.data}>
-        <XAxis dataKey="x">
+        <XAxis
+          dataKey="x"
+          tick={{ fontSize: 12 }}
+          angle={0}
+          domain={[`dataMin - ${offset}`, `dataMax + ${offset}`]}
+          tickFormatter={
+            props.xIsdate
+              ? (x) =>
+                  DateTime.fromSeconds(x).toFormat(
+                    xRange < 60 * 60 * 24
+                      ? "hh:mm"
+                      : xRange < 60 * 60 * 24 * 365
+                      ? "MM-dd"
+                      : "yyyy-MM-dd"
+                  )
+              : undefined
+          }
+        >
           <Label
             value={props.xLabel || ""}
             offset={-5}
@@ -153,14 +244,23 @@ export function extractGraphData(data: string): GraphData | null {
     } else if (xMatches.length > 0 && yMatches.length > 0) {
       const xLabel = xMatches[0];
       const yLabel = yMatches[0];
+
+      const dateParseRes = attemptDatetimeConversion(
+        array.map((obj) => obj[xLabel])
+      );
+
+      const x = dateParseRes ? dateParseRes : array.map((obj) => obj[xLabel]);
+      const data = [];
+      for (let i = 0; i < x.length; i++) {
+        data.push({ x: x[i], y: array[i][yLabel] });
+      }
+
       return {
-        data: array.map((obj) => ({
-          x: obj[xLabel],
-          y: obj[yLabel],
-        })),
+        data,
         xLabel,
         yLabel,
         graphTitle: arrayKey,
+        xIsdate: dateParseRes !== null,
       };
     }
   }
