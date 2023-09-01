dd5125871b89a3875445275e21daf532e1e49162
henry-pulver <henrypulver@hotmail.co.uk>
2023-09-01 13:59:51 +0100

    Add a new page on the examples repo to show Chat component

---
+++ examples/nextjs/src/app/chat/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { SuperflowsButton, SuperflowsChat } from "@superflows/chat-ui-react";
+import { Cog6ToothIcon } from "@heroicons/react/24/outline";
+
+if (!process.env.NEXT_PUBLIC_SUPERFLOWS_API_KEY) {
+  throw new Error(
+    "You must provide a Superflows API key in the environment variables as NEXT_PUBLIC_SUPERFLOWS_API_KEY",
+  );
+}
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen w-1/2 flex-col items-center justify-between p-12 pt-16 pb-2">
+      <SuperflowsChat
+        superflowsApiKey={process.env.NEXT_PUBLIC_SUPERFLOWS_API_KEY ?? ""}
+        userDescription={process.env.NEXT_PUBLIC_USER_DESCRIPTION}
+        userApiKey={process.env.NEXT_PUBLIC_USER_API_KEY}
+        // AIname={"Integrated Finance"}
+        styling={
+          {
+            // type: "modal",
+            // slideoverSide: "right",
+            // buttonColor: "green",
+            // modalClasses: "bg-gray-50",
+            // slideoverSide: "right",
+            // headerTextColor: "#146ef5",
+            // buttonColor: "#146ef5",
+          }
+        }
+        welcomeText={"Welcome to Acme CRM"}
+        suggestions={process.env.NEXT_PUBLIC_SUGGESTIONS?.split(",")}
+      />
+    </main>
+  );
+}
