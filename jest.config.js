3341e392085bc2bcffdd0d86f395af8aecff811e
neuromantic99 <jrowlandm@gmail.com>
2023-09-05 16:14:09 +0100

    fix issue with jest and   "type": "module"

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: "ts-jest/presets/default-esm",
-  testEnvironment: "node",
-  extensionsToTreatAsEsm: [".ts"],
-  transform: {},
-  transformIgnorePatterns: [],
-};
