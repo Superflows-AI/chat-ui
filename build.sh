2443f52859047542a522cbcfda1b5527ddd8abc8
henry-pulver <henrypulver@hotmail.co.uk>
2023-10-20 17:04:54 +0100

    Fix build step on Linux

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
+sed_command="sed -i"
+
+if [ "$(uname)" == "Darwin" ]; then
+    # macOS
+    sed_command="sed -i ''"
+fi
+
 rollup -c
 mv dist/src/index.d.ts dist/index.d.ts
-sed -i '' 's/.\/components/.\/src\/components/g' dist/index.d.ts
-sed -i '' 's/.\/lib/.\/src\/lib/g' dist/index.d.ts
+$sed_command 's/.\/components/.\/src\/components/g' dist/index.d.ts
+$sed_command 's/.\/lib/.\/src\/lib/g' dist/index.d.ts
