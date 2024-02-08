fcf175cd46bfbeaf6a85d930ae9496da7ef021b1
henry-pulver <henrypulver@hotmail.co.uk>
2024-02-08 11:09:48 +0000

    Remove tailwind base
    Instead, apply those styles to all elements inside elements with superflows-style class.

--- src/components/superflowsButton.tsx
+++ src/components/superflowsButton.tsx
@@ -9,7 +9,7 @@ import { ButtonProps } from "../lib/types";
 export default function SuperflowsButton(props: ButtonProps) {
   const [open, setOpen] = useState(false);
   return (
-    <>
+    <div className={"superflows-styling"}>
       {props.styling?.solidIcon ? (
         <SparklesSolidIcon
           className={`sf-p-1 focus:sf-outline-none sf-cursor-pointer sf-bg-transparent sf-text-gray-500 hover:sf-text-[#146ef5] sf-h-7 sf-w-7 ${props.buttonStyling}`}
@@ -28,6 +28,6 @@ export default function SuperflowsButton(props: ButtonProps) {
       ) : (
         <SuperflowsModal open={open} setOpen={setOpen} {...props} />
       )}
-    </>
+    </div>
   );
 }
